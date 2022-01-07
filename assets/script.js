81e9ba0b270efe06a152a1354616c94404d0a688
Hala <hala1286@live.com>
2022-01-07 07:23:50 -0500

    almost done Just to chck and help in some points

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,19 @@ $(document).ready(function(){
         }else{
             $(`.navbar`).removeClass("sticky");
         }
-    })
+        if(this.scrollY > 500){
+           $(`.scroll-up-btn`).addClass("show");
+        }else{
+           $(`scroll-up-btn`).removeClass("show");
+    }
 });  
 
+$(`.scroll-up-btn`).click(function(){
+    $(`html`).animate({scrollTop: 0});
+});
 /*  navbar   */
 $(`.menu-icon`).click(function(){
-$(`.navbar .menu`).toggleClass("active");
+  $(`.navbar .menu`).toggleClass("active");
 $(`.menu-icon i`).toggleClass("active");
+});
 });
