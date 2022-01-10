028d8c99f2a331d741a90b70786e42e05d7f0f06
Hala <hala1286@live.com>
2022-01-09 22:31:14 -0500

    deleting extra script

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
+    
     $("#moreinfo_skills").hide();
     $("#readmore_skills").show()
-    $("#readmore_skills").click(function(e){
+    $("#readmore_skills").click(function(){
         $("#readmore_skills").hide()
         $("#moreinfo_skills").show();
-    })
+    });
 
-    $("#readless_skills").click(function(e){
+    $("#readless_skills").click(function(){
         $("#moreinfo_skills").hide();
         $("#readmore_skills").show()
-    })
-
+    });
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $(`.navbar`).addClass("sticky");
@@ -20,16 +20,21 @@ $(document).ready(function() {
         if(this.scrollY > 500){
            $(`.scroll-up-btn`).addClass("show");
         }else{
-           $(`scroll-up-btn`).removeClass("show");
-    }
-});  
+           $(`.scroll-up-btn`).removeClass("show");
+             }
+    });  
 
-$(`.scroll-up-btn`).click(function(){
-    $(`html`).animate({scrollTop: 0});
-});
-/*  navbar   */
-$(`.menu-icon`).click(function(){
-  $(`.navbar .menu`).toggleClass("active");
-$(`.menu-icon i`).toggleClass("active");
-});
+    $(`.scroll-up-btn`).click(function(){
+       $(`html`).animate({scrollTop: 0});
+        $('html').css("scrollBehavior", "auto");
+     });
+     $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+     /*  navbar   */
+     $(`.menu-icon`).click(function(){
+        $(`.navbar .menu`).toggleClass("active");
+        $(`.menu-icon i`).toggleClass("active");
+     });
 });
